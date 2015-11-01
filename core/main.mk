84364df425c913dfb9879be18d2ecbba67f523b0
Snuzzo <jrracinfan@gmail.com>
2015-11-01 13:10:22 -0500

    Clean up variants & ARCH declaration

--- core/main.mk
+++ core/main.mk
@@ -1,3 +1,25 @@
+ifeq ($(ARCH),)
+ARCH := arm
+endif
+
+ifneq ($(TARGET_REQUIRES_DTB),)
+ifeq ($(DTB_DIR),)
+DTB_DIR := arch/arm/boot
+endif
+endif
+
+ifeq ($(ARCH),arm64)
+ifeq ($(build_type),bootimg)
+	TARGET_ZIMAGE := Image.gz
+else
+	TARGET_ZIMAGE := Image
+endif
+else
+	TARGET_ZIMAGE := zImage
+endif
+
+ZIMAGE := arch/$(ARCH)/boot/$(TARGET_ZIMAGE)
+
 .PHONY: kernel
 kernel:
 	@echo "\033[32m Starting build \033[0m"
@@ -35,28 +57,33 @@ buildbootimg:
 
 .PHONY: printcompletion
 printcompletion:
+ifeq ($(build_type),kernel)
+	@echo
+	@echo "=-=-=-= Complete =-=-=-="
+	@echo "\033[32m $(OUT_DIR)/$(RENDER_PRODUCT)/zImage built successful\033[0m"
+else
 	$(hide) md5sum ./Zip-Files/$(RENDER_PRODUCT)/$(PACKAGE_TARGET_NAME) | sed 's\./Zip-Files/$(RENDER_PRODUCT)/\\' > ./Zip-Files/$(RENDER_PRODUCT)/$(PACKAGE_TARGET_NAME).md5
 	@echo
 	@echo "=-=-=-= Complete =-=-=-="
 	@echo "\033[32m ./Zip-Files/$(RENDER_PRODUCT)/$(PACKAGE_TARGET_NAME) built successful\033[0m"
 	@echo "md5: `cat ./Zip-Files/$(RENDER_PRODUCT)/$(PACKAGE_TARGET_NAME).md5 | cut -d ' ' -f 1`"
+endif
 
 build_type := $(filter kernel anykernel bootimg,$(TARGET_BUILD_VARIANT))
-ifeq ($(build_type),anykernel)
-.PHONY: render
-render:	kernel buildzip printcompletion
-endif
+
+build_type_args := kernel
 ifeq ($(build_type),bootimg)
-.PHONY: render
-render:	kernel buildbootimg buildzip printcompletion
+build_type_args += $(build_type_args) buildbootimg buildzip printcompletion
+endif
+ifeq ($(build_type),anykernel)
+build_type_args += $(build_type_args) buildzip printcompletion
 endif
 ifeq ($(build_type),kernel)
-.PHONY: render
-render:	kernel
-	@echo
-	@echo "=-=-=-= Complete =-=-=-="
-	@echo "\033[32m $(OUT_DIR)/$(RENDER_PRODUCT)/zImage built successful\033[0m"
+build_type_args += $(build_type_args) printcompletion
 endif
+
+.PHONY: render
+render:	$(build_type_args)
 
 BUILD_SYSTEM := $(TOPDIR)build/core
 
@@ -96,25 +123,3 @@ else
 OUT_DIR := $(OUT_DIR_COMMON_BASE)/$(notdir $(PWD))
 endif
 endif
-
-ifeq ($(ARM),)
-ARM := arm
-endif
-
-ifneq ($(TARGET_REQUIRES_DTB),)
-ifeq ($(DTB_DIR),)
-DTB_DIR := arch/arm/boot
-endif
-endif
-
-ifeq ($(ARCH),arm64)
-ifeq ($(build_type),bootimg)
-	TARGET_ZIMAGE := Image.gz
-else
-	TARGET_ZIMAGE := Image
-endif
-else
-	TARGET_ZIMAGE := zImage
-endif
-
- ZIMAGE := arch/$(ARCH)/boot/$(TARGET_ZIMAGE)
